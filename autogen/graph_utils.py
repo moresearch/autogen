1124a87d8b2162ff0a7c47e0384de6155c759854
maf <uni.m.a.fouad@gmail.com>
2024-05-08 13:38:56 -0300

    Adds export_path functionality

--- autogen/graph_utils.py
+++ autogen/graph_utils.py
@@ -107,30 +107,38 @@ def invert_disallowed_to_allowed(disallowed_speaker_transitions_dict: dict, agen
             agent for agent in allowed_speaker_transitions_dict[key] if agent not in value
         ]
 
-    return allowed_speaker_transitions_dict
+	return allowed_speaker_transitions_dict
 
 
-def visualize_speaker_transitions_dict(speaker_transitions_dict: dict, agents: List[Agent]):
-    """
-    Visualize the speaker_transitions_dict using networkx.
-    """
-    try:
-        import matplotlib.pyplot as plt
-        import networkx as nx
-    except ImportError as e:
-        logging.fatal("Failed to import networkx or matplotlib. Try running 'pip install autogen[graphs]'")
-        raise e
+def visualize_speaker_transitions_dict(
+	speaker_transitions_dict: dict,
+	agents: List[Agent], 
+	export_path: Optional[str] = None
+):
+	"""
+	Visualize the speaker_transitions_dict using networkx.
+	"""
+	try:
+		import matplotlib.pyplot as plt
+		import networkx as nx
+	except ImportError as e:
+		logging.fatal("Failed to import networkx or matplotlib. Try running 'pip install autogen[graphs]'")
+		raise e
 
-    G = nx.DiGraph()
+	G = nx.DiGraph()
 
-    # Add nodes
-    G.add_nodes_from([agent.name for agent in agents])
+	# Add nodes
+	G.add_nodes_from([agent.name for agent in agents])
 
-    # Add edges
-    for key, value in speaker_transitions_dict.items():
-        for agent in value:
-            G.add_edge(key.name, agent.name)
+	# Add edges
+	for key, value in speaker_transitions_dict.items():
+		for agent in value:
+			G.add_edge(key.name, agent.name)
 
-    # Visualize
-    nx.draw(G, with_labels=True, font_weight="bold")
-    plt.show()
+	# Visualize
+	nx.draw(G, with_labels=True, font_weight="bold")
+
+	if export_path is not None:
+		plt.savefig(export_path)
+	else:
+		plt.show()
